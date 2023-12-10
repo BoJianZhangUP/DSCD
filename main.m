4e716d7106a5df8baab0880d5b9599a233a77495
zhangbojian666 <64386516+zhangbojian666@users.noreply.github.com>
2023-12-10 15:59:31 +0800

    Update main.m

--- main.m
+++ main.m
@@ -11,39 +11,24 @@ qe=10;
 switch image_num
     case {'5'}
         test_set='oxford5k';
+        query_set='oxford5k';
     case {'6'}
         test_set='paris6k';
+        query_set='paris6k';
     case {'r5'}
         test_set='roxford5k';
+        query_set='roxford5k';  
     case {'r6'}
         test_set='rparis6k';
+        query_set='rparis6k'; 
     case {'106'}
         test_set='paris106k';
+        query_set='paris6k';
+        query_files = dir(fullfile(datapath,net,'\datasets\',['paris6k','_nquery_name'],'*.mat'));
     case {'105'}
         test_set='oxford105k';
-end
-
-switch test_set
-    case {'oxford5k'}
-        train_set2='paris6k'; % traditional PCA-whitening dataset
         query_set='oxford5k';
-    case {'paris6k'}
-        train_set2='oxford5k';
-        query_set='paris6k';
-    case {'roxford5k'}
-        train_set2='rparis6k';
-        query_set='roxford5k';   
-    case {'rparis6k'}
-        train_set2='roxford5k';
-        query_set='rparis6k';        
-    case {'oxford105k'}
-        query_set='oxford5k';
-        train_set2='paris6k';
-        query_files = dir(fullfile(datapath,netname,'\pool5\',['oxford5k','_query'],'*.mat'));
-    case {'paris106k'}
-        query_set='paris6k';
-        train_set2='oxford5k';
-        query_files = dir(fullfile(datapath,netname,'\pool5\',['paris6k','_query'],'*.mat'));
+        query_files = dir(fullfile(datapath,net,'\datasets\',['oxford5k','_nquery_name'],'*.mat'));
 end
 
 
