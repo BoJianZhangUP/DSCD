6d017c326fc84514e05ab312c91d396e0046d6df
BoJianZhangUP <64386516+BoJianZhangUP@users.noreply.github.com>
2023-12-12 17:08:18 +0800

    Update display_rquery_results.m

--- display_rquery_results.m
+++ display_rquery_results.m
@@ -9,7 +9,7 @@ for m=1:7
     [PW_test_features_pca,PW_query_nocrop_features_pca]=APW_whitening(train_features_normalize,test_features_normalize,query_nocrop_features_normalize,dim);
 
     %%%%%%% APW %%%%%%
-    [PWP_test_features_pca,APW_query_nocrop_features_pca]=APW_whitening(train_features_normalize,test_features_normalize,query_nocrop_features_normalize,dim,'APW');
+    [APW_test_features_pca,APW_query_nocrop_features_pca]=APW_whitening(train_features_normalize,test_features_normalize,query_nocrop_features_normalize,dim,'APW');
 
 
     PW_dist=pdist2(PW_test_features_pca,PW_query_nocrop_features_pca,'euclidean');
@@ -17,7 +17,7 @@ for m=1:7
     compute_r_map(dim,gnd,PW_ranks,test_set,'nocrop','PW_nocrop__map');
 
 
-    dist=pdist2(PWP_test_features_pca,APW_query_nocrop_features_pca,'euclidean');
+    dist=pdist2(APW_test_features_pca,APW_query_nocrop_features_pca,'euclidean');
     [~, APWcrop_ranks] = sort(dist, 'ascend');
     compute_r_map(dim,gnd,APWcrop_ranks,test_set,'nocrop','APW_nocrop__map');
 
