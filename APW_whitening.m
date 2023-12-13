cbbe592a57da4e8eb6789b32892161778bb293a5
BoJianZhangUP <64386516+BoJianZhangUP@users.noreply.github.com>
2023-12-13 16:08:12 +0800

    Update APW_whitening.m

--- APW_whitening.m
+++ APW_whitening.m
@@ -12,7 +12,6 @@ if nargin==5
     test_feature_pca=scoreTrain11(:,1:dim);
     test_feature_pca=normalize(test_feature_pca,2,"norm");
 
-
     q_features=normalize(query_features,2,"norm");
     query_features_white=query_pca(q_features,coeff,mu,u,s,size(PW_feature,2));
 
@@ -23,8 +22,6 @@ if nargin==5
     q_features=(query_features_white-mu11)*coeff11;
     query_feature_pca=q_features(:,1:dim);
     query_feature_pca=normalize(query_feature_pca,2,"norm");
-
-
 else
     %%%% PW %%%%%
     [PW_feature,coeff,mu,u,s]= pca_and_whitening(train_features_normalize,test_features_normalize,dim);
